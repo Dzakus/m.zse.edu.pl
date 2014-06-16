953e23271a804676af63621daea31731c43be27c
Kamil <kprosciewicz@gmail.com>
2014-06-16 06:07:16 +0200

    postep nad funkcja responsywnej tabeli zastepstw

--- app/helpers/Minify.php
+++ app/helpers/Minify.php
@@ -23,23 +23,36 @@ class Minify {
     }
 
     static function table_responsive($file){
-        $html = file_get_contents("http://szkola.zse.edu.pl/zastepstwa/".$file);
 //        $cleared = preg_replace("/<([a-z][a-z0-9]*)[^>]*?(\/?)>/i",'<$1$2>', $html);
-        $singles = preg_split('/<tr>
-<td nowrap="" class="st17" align="LEFT">
-&nbsp;
-<\/td>
-<td nowrap="" class="st17" align="LEFT">
-&nbsp;
-<\/td>
-<td nowrap="" class="st17" align="LEFT">
-&nbsp;
-<\/td>
-<td nowrap="" class="st17" align="LEFT">
-&nbsp;
-<\/td>
-<\/tr>/', $html);
-//        $singles = explode('<tr><td nowrap="" class="st17" align="LEFT">&nbsp;</td><td nowrap="" class="st17" align="LEFT">&nbsp;</td><td nowrap="" class="st17" align="LEFT">&nbsp;</td><td nowrap="" class="st17" align="LEFT">&nbsp;</td></tr>', $html);
-        return $singles;
+        $dom = new DOMDocument();
+        libxml_use_internal_errors(true);
+        //load the html
+        $html = $dom->loadHTMLFile("http://szkola.zse.edu.pl/zastepstwa/".$file);
+        libxml_clear_errors();
+        //discard white space
+        $dom->preserveWhiteSpace = false;
+
+        //the table by its tag name
+        $tables = $dom->getElementsByTagName('table');
+
+        //get all rows from the table
+        $rows = $tables->item(0)->getElementsByTagName('tr');
+        $cols = null;
+        $buffers = [];
+        foreach ($rows as $row)
+        {
+            $cols = $row->getElementsByTagName('td');
+            if($cols->length == 4){
+                $buffer = '';
+                foreach($cols as $col){
+                    $buffer.=$col->getAttribute('class');
+                }
+                if(preg_match("/(st17){4}/", $buffer)){
+                    array_push($buffers, $buffer);
+                }
+            }
+        }
+        return $buffers;
+//        return $singles;
     }
 } 
--- app/views/substitutes/index.blade.php
+++ app/views/substitutes/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('content')
 <div class="table-responsive">
-    {{ dd(Minify::table_responsive($sub->plik)) }}
+    {{ print_r(Minify::table_responsive($sub->plik)) }}
 </div>
 @stop
