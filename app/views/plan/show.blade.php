6aef4b5ba4b287675a36dda5211aaa17042a0673
Kamil <kprosciewicz@gmail.com>
2014-10-20 13:56:35 +0200

    little modifications

--- app/views/plan/show.blade.php
+++ app/views/plan/show.blade.php
@@ -4,5 +4,4 @@
 	    	echo "<h3>".$plan->klasa."</h3>";
 	    	echo Responsive::plan($plan->plik, preg_match("/.*religia.*/", strtolower($plan->klasa)));
 	    ?>
-	    <div class="mobile"></div>
 @stop
